154f2edf75931164890cdadc082eae04ae839c00
edgarrd39 <rodrigo.09tuc@gmail.com>
2023-06-04 23:45:24 -0300

    Agrego Poncho - Agrego archivos Pantalla vacio

--- src/pantalla.c
+++ src/pantalla.c
@@ -19,83 +19,19 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 SPDX-License-Identifier: MIT
 *************************************************************************************************/
 
-#ifndef CIAA_H
-#define CIAA_H
-
-/** \brief Board Hardware Support (BSP)
+/** \brief Digital inputs/outputs
  **
- ** Este módulo se encarga de proporcionar las configuraciones de entrada y salida para la placa EDU-CIIA-NXP
  **
- ** \addtogroup bsp BSP
- ** \brief Board Hardware Support
+ ** \addtogroup hal HAL
+ ** \brief Hardware Abstraction layer
  ** @{ */
 
 /* === Headers files inclusions =============================================================== */
 
-#include "bsp.h"
+#include "pantalla.h"
 #include "chip.h"
 
 /* === Macros definitions ====================================================================== */
-
-#define LED_R_PORT 2
-#define LED_R_PIN 0
-#define LED_R_FUNC SCU_MODE_FUNC4
-#define LED_R_GPIO 5
-#define LED_R_BIT 0
-
-#define LED_G_PORT 2
-#define LED_G_PIN 1
-#define LED_G_FUNC SCU_MODE_FUNC4
-#define LED_G_GPIO 5
-#define LED_G_BIT 1
-
-#define LED_B_PORT 2
-#define LED_B_PIN 2
-#define LED_B_FUNC SCU_MODE_FUNC4
-#define LED_B_GPIO 5
-#define LED_B_BIT 2
-
-#define LED_1_PORT 2
-#define LED_1_PIN 10
-#define LED_1_FUNC SCU_MODE_FUNC0
-#define LED_1_GPIO 0
-#define LED_1_BIT 14
-
-#define LED_2_PORT 2
-#define LED_2_PIN 11
-#define LED_2_FUNC SCU_MODE_FUNC0
-#define LED_2_GPIO 1
-#define LED_2_BIT 11
-
-#define LED_3_PORT 2
-#define LED_3_PIN 12
-#define LED_3_FUNC SCU_MODE_FUNC0
-#define LED_3_GPIO 1
-#define LED_3_BIT 12
-
-#define TEC_1_PORT 1
-#define TEC_1_PIN 0
-#define TEC_1_FUNC SCU_MODE_FUNC0
-#define TEC_1_GPIO 0
-#define TEC_1_BIT 4
-
-#define TEC_2_PORT 1
-#define TEC_2_PIN 1
-#define TEC_2_FUNC SCU_MODE_FUNC0
-#define TEC_2_GPIO 0
-#define TEC_2_BIT 8
-
-#define TEC_3_PORT 1
-#define TEC_3_PIN 2
-#define TEC_3_FUNC SCU_MODE_FUNC0
-#define TEC_3_GPIO 0
-#define TEC_3_BIT 9
-
-#define TEC_4_PORT 1
-#define TEC_4_PIN 6
-#define TEC_4_FUNC SCU_MODE_FUNC0
-#define TEC_4_GPIO 1
-#define TEC_4_BIT 9
 
 /* === Private data type declarations ========================================================== */
 
@@ -114,5 +50,3 @@ SPDX-License-Identifier: MIT
 /* === End of documentation ==================================================================== */
 
 /** @} End of module definition for doxygen */
-
-#endif /* CIAA_H */
